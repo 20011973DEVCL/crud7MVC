d67fc5f262cd2a1907b22a973676c14c532682d8
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2023-11-16 15:28:56 -0300

    se avanza con codigo html

--- Controllers/InicioController.cs
+++ Controllers/InicioController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using crud7MVC.Models;
+using MagicVilla.Datos;
+using Microsoft.EntityFrameworkCore;
 
 namespace crud7MVC.Controllers;
 
-public class HomeController : Controller
+public class InicioController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _contexto;
+
+    public InicioController(ApplicationDbContext contexto)
     {
-        _logger = logger;
+        this._contexto = contexto;
     }
 
-    public IActionResult Index()
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        return View();
+        return View(await _contexto.Contactos.ToListAsync());
     }
 
     public IActionResult Privacy()
