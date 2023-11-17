f5eb5eeb391c069e778ba4992313a2cc7f61453b
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2023-11-17 16:27:22 -0300

    se crea la accion editar y la vista asociada

--- Controllers/InicioController.cs
+++ Controllers/InicioController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using crud7MVC.Models;
 using MagicVilla.Datos;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace crud7MVC.Controllers;
 
@@ -22,8 +23,54 @@ public class InicioController : Controller
         return View(await _contexto.Contactos.ToListAsync());
     }
 
-    public IActionResult Privacy()
+    [HttpGet]
+    public IActionResult Crear() {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Crear(Contacto contacto )
     {
+        if (ModelState.IsValid)
+        {
+            _contexto.Contactos.Add(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View();
+    }
+    
+
+    [HttpGet]
+    public IActionResult Editar(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var contacto= _contexto.Contactos.Find(id);
+        if (contacto == null)
+        {
+            return NotFound();
+        }
+
+        return View(contacto);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Editar(Contacto contacto)
+    {
+        if (ModelState.IsValid)
+        {
+            _contexto.Update(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         return View();
     }
 
